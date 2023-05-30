559cbae91272bb330fc1cd642e6adf82e554828b
maxivega0 <maximilianovega600@gmail.com>
2023-05-30 12:19:02 -0300

    punto 3 realizado

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 
-const Titulo = ({nuevoTitulo}) => {
-    return (
-        <div>
-            <h1>Hola {nuevoTitulo}</h1>
-        </div>
-    );
+const Titulo = () => {
+
+    const [titulo, setTitulo] = useState("Hola amiguito")
+    const cambiarTitulo = () => {
+        setTitulo("Hola amiguito(con estado modificado)")
+    }
+  return (
+    
+
+
+    <div>
+      <h1>{titulo}</h1>
+      <button onClick={cambiarTitulo}>click</button>
+    </div>
+  );
 };
 
 export default Titulo;
