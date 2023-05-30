da484b12a8e570f1fd95c99d06c97bdb94599e59
maxivega0 <maximilianovega600@gmail.com>
2023-05-30 12:11:04 -0300

    eercicio 1 y 2 resuelto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Titulo from './components/Titulo'
+function App() {
+
+  return (
+    <>
+    {/* <h1>Hola Mundo!</h1> */}
+    <Titulo nuevoTitulo="mi amigo"></Titulo>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,10 @@
+
+const Titulo = ({nuevoTitulo}) => {
+    return (
+        <div>
+            <h1>Hola {nuevoTitulo}</h1>
+        </div>
+    );
+};
+
+export default Titulo;
