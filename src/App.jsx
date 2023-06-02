56eb959c4df524bd2849f265916936dce9a92ca3
maxivega0 <maximilianovega600@gmail.com>
2023-06-01 23:15:47 -0300

    formato al documento

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
-import Titulo from './components/Titulo'
+import "./App.css";
+import Titulo from "./components/Titulo";
 function App() {
-
   return (
     <>
-    {/* <h1>Hola Mundo!</h1> */}
-    <Titulo nuevoTitulo="mi amigo"></Titulo>
+      {/* <h1>Hola Mundo!</h1> */}
+      <Titulo nuevoTitulo="mi amigo"></Titulo>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import Button from 'react-bootstrap/Button';
 
 const Titulo = () => {
-
-    const [titulo, setTitulo] = useState("Hola amiguito")
-    const cambiarTitulo = () => {
-        setTitulo("Hola amiguito(con estado modificado)")
-    }
+  const [titulo, setTitulo] = useState("Hola amiguito");
+  const cambiarTitulo = () => {
+    setTitulo("Hola amiguito(con estado modificado)");
+  };
   return (
-    
-
-
     <div className="text-center">
       <h1>{titulo}</h1>
-      <button onClick={cambiarTitulo} className="btn btn-outline-danger ">click</button>
+      <button onClick={cambiarTitulo} className="btn btn-outline-danger ">
+        click
+      </button>
     </div>
   );
 };
